e16ceed54c32491a0706809a6f8aafbb5a14f260
philip anlander <anlanderp@gmail.com>
2018-11-29 06:00:46 +0100

    done

--- tictac.js
+++ tictac.js
@@ -1,104 +1,114 @@
+$(document).ready(() => {
+    console.log('Ready')
+    let squareOne = $("#s1");
+    let squareTwo = $("#s2");
+    let squareThree = $("#s3");
+    let squareFour = $("#s4");
+    let squareFive = $("#s5");
+    let squareSix = $("#s6");
+    let squareSeven = $("#s7");
+    let squareEight = $("#s8");
+    let squareNine = $("#s9");
+    let tableDetail = $("td");
+    let button = $("button");
+    let winnningDiv = $(".winning");
+    let playTurn = "X";
+    let checkXWin = "X";
+    let checkOWin = "O";
 
-let playTurn = "X"
-
-
-let cell = $(".cell")
-console.log(cell)
-$( window ).load(function(){
-  console.log("ready")
-  playTurn = "X";
-  $("button").click(function(){
-
-    cell.text("")
-    console.log("start", document.turn)
-  })
-})
-
-let tableDetail = $("td")
-    tableDetail.each((index) => {
-        let square = tableDetail[index]
-        $(square).click(() => {
-            nextMove(square)
-            checkWin()
-        })
+    $(button).click(() => {
+        $(tableDetail).text("")
+        $(winnningDiv).text("")
+        $("table").show()
+        playTurn = "X"
     })
+    function checkTurn(square) {
+        if($(square).text() === "") {
+            if(playTurn === "X") {
+                $(square).text("X")
+                playTurn = "O"
+            } else {
+                $(square).text("O")
+                playTurn = "X"
+            }
+        } else {
+            return null
+        }
+    }
 
-  function nextMove(square) {
-      if($(square).text() === "") {
-          if(playTurn === "X") {
-              $(square).text("X")
-              playTurn = "O"
-          } else {
-              $(square).text("O")
-              playTurn = "X"
-          }
-      } else {
-          return null
+    function checkIfXWins() {
+        $("table").hide()
+        $(winnningDiv).text("X wins").css("font-size", "400%");
+    }
+    function checkIfOWins() {
+        $("table").hide()
+        $(winnningDiv).text("O wins").css("font-size", "400%");
+    }
+    function checkWin() {
+        if($(squareOne).text() === checkXWin && $(squareTwo).text() === checkXWin && $(squareThree).text() === checkXWin) {
+            console.log('First X Win')
+            checkIfXWins()
+        } else if($(squareOne).text() === checkOWin && $(squareTwo).text() === checkOWin && $(squareThree).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareFour).text() === checkXWin && $(squareFive).text() === checkXWin && $(squareSix).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareFour).text() === checkOWin && $(squareFive).text() === checkOWin && $(squareSix).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareSeven).text() === checkXWin && $(squareEight).text() === checkXWin && $(squareNine).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareSeven).text() === checkOWin && $(squareEight).text() === checkOWin && $(squareNine).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareOne).text() === checkXWin && $(squareFour).text() === checkXWin && $(squareSeven).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareOne).text() === checkOWin && $(squareFour).text() === checkOWin && $(squareSeven).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareTwo).text() === checkXWin && $(squareFive).text() === checkXWin && $(squareEight).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareTwo).text() === checkOWin && $(squareFive).text() === checkOWin && $(squareEight).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareThree).text() === checkXWin && $(squareSix).text() === checkXWin && $(squareNine).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareThree).text() === checkOWin && $(squareSix).text() === checkOWin && $(squareNine).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareOne).text() === checkXWin && $(squareFive).text() === checkXWin && $(squareNine).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareOne).text() === checkOWin && $(squareFive).text() === checkOWin && $(squareNine).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else if($(squareThree).text() === checkXWin && $(squareFive).text() === checkXWin && $(squareSeven).text() === checkXWin) {
+            console.log('X Win')
+            checkIfXWins()
+        } else if($(squareThree).text() === checkOWin && $(squareFive).text() === checkOWin && $(squareSeven).text() === checkOWin) {
+            console.log('O Win')
+            checkIfOWins()
+        } else {
+            for(let i = 0; i < tableDetail.length; i++) {
+                if($(tableDetail[i]).text() !== "") {
+                    console.log('FOREN')
+                } else {
+                    console.log('else', $(tableDetail[i]).text())
+                }
+            }
+        }
       }
-  }
 
-
-
-var win1 = $('#s1');
-var win2 = $('#s2');
-var win3 = $('#s3');
-var win4 = $('#s4');
-var win5 = $('#s5');
-var win6 = $('#s6');
-var win7 = $('#s7');
-var win8 = $('#s8');
-var win9 = $('#s9');
-
-
-function checkWin() {
-  if ($(win1).text("X") && $(win2).text("X") && $(win3).text("X")) {
-    winner("X");
-  } else if ($(win1).text("O") && $(win2).text("O") && $(win3).text("O")) {
-    winner("O");
-  }
-
-  else if ($(win4).text("X") && $(win5).text("X") && $(win6).text("X")) {
-
-  } else if ($(win4).text("O") && $(win5).text("O") && $(win6).text("O")) {
-
-  }
-
-  else if ($(win7).text("X") && $(win8).text("X") && $(win9).text("X")) {
-
-  } else if ($(win7).text("O") && $(win8).text("O") && $(win9).text("O")) {
-
-  }
-
-  else if ($(win5).text("X") && $(win2).text("X") && $(win8).text("X")) {
-
-  } else if ($(win5).text("O") && $(win2).text("O") && $(win8).text("O")) {
-
-  }
-
-  else if ($(win6).text("X") && $(win9).text("X") && $(win3).text("X")) {
-
-  } else if ($(win6).text("O") && $(win9).text("O") && $(win3).text("O")) {
-
-  }
-
-  else if ($(win1).text("X") && $(win5).text("X") && $(win9).text("X")) {
-
-  } else if ($(win1).text("O") && $(win5).text("O") && $(win9).text("O")) {
-
-  }
-
-  else if ($(win5).text("X") && $(win7).text("X") && $(win3).text("X")) {
-
-  } else if ($(win5).text("O") && $(win7).text("O") && $(win3).text("O")) {
-
-  }
-}
-
-
-
-
-function winner() {
- if (checkWin==true){
-   alert("congrats you won")
- }
-}
+      tableDetail.each((index) => {
+          let square = tableDetail[index]
+          $(square).click(() => {
+              checkTurn(square)
+              checkWin()
+          })
+      })
+  })
